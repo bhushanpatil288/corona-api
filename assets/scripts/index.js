5dcfd394811e29d8b16435bd96118d2bdc3d784e
bhushanpatil288 <sharewithbhushan@gmail.com>
2026-01-12 15:25:51 +0530

    [add] - basic working corona api

---
+++ assets/scripts/index.js
@@ -0,0 +1,65 @@
+const form = document.querySelector("form");
+const stateName = document.querySelector("#stateName");
+const cases = document.querySelector("#cases");
+const discharged = document.querySelector("#discharged");
+const cdeaths = document.querySelector("#cdeaths");
+const statesContainer = document.querySelector("#statesContainer");
+const totalDeaths = document.querySelector("#deaths");
+const totalCases = document.getElementById("total");
+
+document.addEventListener('DOMContentLoaded', function(){
+    (async ()=>{
+        const data = await fetchData();
+        setInitialData(data);
+        listenSubmit(data);
+    })()
+    
+})
+
+async function fetchData(){
+    const bufferData = await fetch('https://api.rootnet.in/covid19-in/stats/latest');
+    const jsonData = await bufferData.json();
+    
+    return jsonData;
+}
+
+function setInitialData(data){
+    totalCases.innerHTML = data.data.summary.total;
+    totalDeaths.innerHTML = data.data.summary.deaths;
+    statesContainer.innerHTML = ''
+    data.data.regional.forEach((region, idx)=>{
+        statesContainer.innerHTML += `
+             <div class="col-md-6 col-xl-3">
+                    <div class="card mt-5 shadow rounded rounded-3 p-3 pb-5 text-center h-100 d-flex justify-content-between">
+                        <h2 id="stateName">${region.loc}</h2>
+                        <div>
+                             <div class="d-flex gap-1">
+                                <p class="m-0">Confirmed cases: </p>
+                                <p class="m-0" id="cases">${region.confirmedCasesIndian}</p>
+                            </div>
+                            <div class="d-flex gap-1">
+                                <p class="m-0">Discharged: </p>
+                                <p class="m-0" id="discharged">${region.discharged}</p>
+                            </div>
+                            <div class="d-flex gap-1">
+                                <p class="m-0">Confirmed deaths: </p>
+                                <p class="m-0" id="cdeaths">${region.deaths}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        `
+    })
+}
+
+function listenSubmit(data){
+    form.addEventListener('submit', function(e){
+        e.preventDefault();
+        const input = document.querySelector("#stateInput").value;
+        const stateData = data.data.regional.filter(state => state.loc.toLowerCase() === input.toLowerCase());
+        stateName.innerHTML = stateData[0].loc;
+        cases.innerHTML = stateData[0].confirmedCasesIndian;
+        discharged.innerHTML = stateData[0].discharged;
+        cdeaths.innerHTML = stateData[0].deaths;
+    })
+}
